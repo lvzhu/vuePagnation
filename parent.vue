f58cb657e9f8970754f6f417790178d1515d131b
lvzhu <1041433396@qq.com>
2018-06-11 18:41:57 +0800

    Create parent.vue

---
+++ parent.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="group-info">
+			
+      <!-- 分页 -->
+			<MoPaging class="myadd-pag" :jumpShow="isShowJump" :pageIndex="currentPage" :total="count" :pageSize="pageSize" @change="pageChange"></MoPaging>
+
+	</div>
+</template> 
+
+<script>
+import MoPaging from './pageNation'
+export default {
+	components: {
+		MoPaging
+	},
+	data() {
+		return {
+	
+			//分页组件数据
+			pageSize: 10, //每页显示10条数据
+			currentPage: 1, //当前页码
+			count: 0, //总记录数
+			isShowJump:true,  //是否显示跳页
+		}
+	},
+	methods: {
+		//从page组件传递过来的当前page
+		pageChange(page) {
+			this.currentPage = page;
+			// this.getList();  //查询数据
+		}
+	}
+}
+
+</script>
+
+<style type="text/css">
+
+</style>
